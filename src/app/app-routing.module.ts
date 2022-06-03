0333fffd1ca9459597e09c95266ed0d2893c61ad
leiner.barrios@inglesparatodos.edu.co <devappipt@Devs-iMac.local>
2022-06-03 11:37:34 -0500

    Routes and subroutes finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,14 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'avengers',
+    loadChildren: () => import('./avengers/avengers.module').then(m => m.AvengersModule)
+  },
+  {
     path: '404',
     component: ErrorPageComponent
   },
--- src/app/avengers/avengers.module.ts
+++ src/app/avengers/avengers.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { AvengersRoutingModule } from './avengers-routing.module';
+
 import { AddComponent } from './pages/add/add.component';
 import { SearchComponent } from './pages/search/search.component';
 import { AvengerComponent } from './pages/avenger/avenger.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
+import { EditComponent } from './pages/edit/edit.component';
 
 
 
@@ -14,10 +18,12 @@ import { ListComponent } from './pages/list/list.component';
     SearchComponent,
     AvengerComponent,
     HomeComponent,
-    ListComponent
+    ListComponent,
+    EditComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AvengersRoutingModule
   ]
 })
 export class AvengersModule { }
