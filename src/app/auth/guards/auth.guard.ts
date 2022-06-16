25f560111d07c2bb149a06e0982d3e667c8183a3
leiner.barrios@inglesparatodos.edu.co <devappipt@Devs-iMac.local>
2022-06-16 15:26:42 -0500

    localStorage

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
@@ -8,21 +9,38 @@ import { AuthService } from '../services/auth.service';
 })
 export class AuthGuard implements CanActivate, CanLoad {
 
-  constructor( private authService: AuthService ) {}
+  constructor( private authService: AuthService, private router: Router ) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-      if (this.authService.auth.id) {
-        return true
-      }
-    return false;
+      return this.authService.verificateAuthentication().pipe(
+        tap( isAuthenticated => {
+          if (!isAuthenticated) {
+            this.router.navigate(['./auth/login'])
+          }
+        })
+      )
+    //   if (this.authService.auth.id) {
+    //     return true
+    //   }
+    // return false;
   }
+
   canLoad(
     route: Route,
-    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-      if (this.authService.auth.id) {
-        return true
-      }
-    return false;
-  }
+    segments: UrlSegment[]): Observable<boolean> | boolean {
+      return this.authService.verificateAuthentication().pipe(
+        tap( isAuthenticated => {
+          if (!isAuthenticated) {
+            this.router.navigate(['./auth/login'])
+          }
+        })
+      )
+    }
+
+    //   if (this.authService.auth.id) {
+    //     return true
+    //   }
+    // return false;
 }
